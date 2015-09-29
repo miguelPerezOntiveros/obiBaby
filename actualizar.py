d5720dc05943907b2317949267943c94c2289145
miguelPerezOntiveros <miguel@miguels-MacBook-Pro.local>
2015-09-29 15:28:07 -0500

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 09f4f843dea274bdebd58e7952e340dfa9851902, reversing
    changes made to 08b51f36a41b80cd1e8d06ce3fa9cc00c7241679.

--- actualizar.py
+++ actualizar.py
@@ -4,17 +4,16 @@ import MySQLdb
 import time
 import os
 
-
 #Subprocess, es para correr comandos
 import subprocess
-#Es una funcion obtenida de esta liga
+#Es una funcion obtenida de esta liga 
 #http://stackoverflow.com/questions/4760215/running-shell-command-from-python-and-capturing-the-output
 #Obtiene lo que devuelve el comando.
-def runProcess(exe):
+def runProcess(exe):    
     p = subprocess.Popen(exe, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     while(True):
-      retcode = p.poll()
-      line = p.stdout.readline
+      retcode = p.poll() 
+      line = p.stdout.readline()
       yield line
       if(retcode is not None):
         break
@@ -45,13 +44,13 @@ while True:
 		print "Leyendo Temperatura y humedad"
 	        [temp,humidity] = grovepi.dht(sensorth,0)
 		print "temp =", temp, " humidity =", humidity
-		cur.execute("INSERT INTO log_user_temperature (temperature, humidity) VALUES ('%s', '%s')", (temp, humidity))
+		cur.execute("INSERT INTO log_user_temperature (temperature, humidity, created_at) VALUES ('%s', '%s', %s)", (temp, humidity, fecha))
 		db.commit()
 		print "Temperatura y Humedad Actualizado en la BD"
 		print "Leyendo sensor de Gas"
 		sensor_value = grovepi.analogRead(gas_sensor)
 		print "sensor_value =", sensor_value
-		cur.execute("INSERT INTO log_user_gas (co) VALUES ('%s')", (sensor_value))
+		cur.execute("INSERT INTO log_user_gas (co, created_at) VALUES ('%s', %s)", (sensor_value, fecha))
 		db.commit()
 		print "Sensor de Gas Actualizado en la BD"
 		print "Leyendo sensor de Aire"
@@ -66,12 +65,12 @@ while True:
 	            air = 1
 
 	        print "sensor_valueAir =", sensor_valueAir, " Aire =", air
-		cur.execute("INSERT INTO log_user_air (value, pollution) VALUES ('%s', '%s')", (sensor_valueAir, air))
+		cur.execute("INSERT INTO log_user_air (value, pollution, created_at) VALUES ('%s', '%s', %s)", (sensor_valueAir, air, fecha))
 		db.commit()
 		print "Sensor de Aire Actualizado en la BD"
 		print "Leyendo sensor de Movimiento"
-		if grovepi.digitalRead(pir_sensor):
-		    	cur.execute("INSERT INTO log_user_motion (time) VALUES (%s)", (fecha))
+		if grovepi.digitalRead(pir_sensor):		
+		    	cur.execute("INSERT INTO log_user_motion (time, created_at) VALUES (%s, %s)", (fecha, fecha))
 			db.commit()
 			print "Sensor de Movimiento Actualizado en la BD"
 	        else:
@@ -79,26 +78,8 @@ while True:
 
 
 		print "--------------------------------------------------"
+		
 
-
-<<<<<<< HEAD
-=======
-		####################################################################################################
-		#Parte en la que se realiza la configuracion de la Raspberry si solo si esta conectada por Ethernet.
-
-		#Se revisa si esta conectado via Ethernet...
-		for line in runProcess('ifconfig eth0 | grep inet'.split()):
-		#Aca se realizaria la configuracion...
-			print "Estoy conectado via Ethernet, y soy bien chido. Aca me configuro"
-			#hacer query, nada mas asignar varibales por ahora
-			cell = Cell.all('wlan0')[0]
-			scheme = Scheme.for_cell('wlan0', 'talivan0009', "jiko1234")
-			scheme.save()
-			scheme.activate()
-			#esperar a que de verdad se haya conectado, y despues seguir
-			break
-
->>>>>>> origin/master
 		#varibale que guarde el tiempo
 		#if(varible + 2 < time.time()) corre ciclo else sleep(0.1);
 		#Obtiene el tiempo actual, para al final utilizarlo y medir cuanto falta para que sean 2 segundos
@@ -108,3 +89,4 @@ while True:
 		time.sleep( millis )
 	except IOError:
 		print "Error"
+
